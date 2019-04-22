0f5c1376c63a1c96c4d8f3645e72098cb7e43475
phpfm <2277357862@qq.com>
2019-04-22 21:47:46 +0800

    msg

---
+++ 20190416/20190417/l.php
@@ -0,0 +1,9 @@
+<?php
+$txt1="Learn PHP";
+$txt2="w3cschool.cc";
+$cars=array("Volvo","BMW","Toyota");
+echo $txt1;
+echo "<br>";
+echo "Study PHP at $txt2";
+echo "My car is a{$cars[0]}";
+echo $txt1+$txt2;
