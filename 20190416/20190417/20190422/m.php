d0eca7949bad292880d610811a3ef4af572c4214
phpfm <2277357862@qq.com>
2019-04-30 17:34:11 +0800

    files

---
+++ 20190416/20190417/20190422/m.php
@@ -0,0 +1,5 @@
+<?php
+print "<h2>php is fun!</h2>";
+print "Hello world!<br>";
+print "i'm about to learn PHP!";
+?>
---
+++ 20190416/20190417/20190422/n.php
@@ -0,0 +1,6 @@
+<?php
+$txt1="Learn PHP";
+$txt2="w3cschool.cc";
+$cars=array("Volvo","BMW","Toyota");
+echo $txt1;
+echo $txt2;
